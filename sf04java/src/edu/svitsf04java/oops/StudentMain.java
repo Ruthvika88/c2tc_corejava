3a5a76afeee36561f5444540af0bf654e4d6c388
hp <hp@Ruthvika>
2024-06-06 15:46:31 +0530

    first commit

---
+++ sf04java/src/edu/svitsf04java/oops/StudentMain.java
@@ -0,0 +1,13 @@
+package edu.svitsf04java.oops;
+import java.util.Scanner;
+public class StudentMain 
+{
+	public static void main(String[]args)
+	{
+		Student studentone = new Student();
+		System.out.println(studentone);
+	}
+
+	Student studentone = new Student(101,"rakesh","CSE","BANGLORE");
+    
+}
